9714e9b2a01de3365bbb5429312612a4969ab74f
Agustin Camino <agustin.camino@gmail.com>
2016-10-11 17:02:17 -0500

    Bump Twilio library to version 5.x
    - Refactor notification controller
      * Enclose phone numbers with quotes on config/administrators.yml
      * Fix tests. The previous one did real requests.
      * Remove unnecessary instance variables.
    - Add .env to .gitignore
    - Add gemset files
    - Enable CI
    - Remove database dependency
      * This application does not require to use a database.
    - Update local development instructions
      * Removed "Deploy to Heroku" Button: The notification relies on a list of
        aministrators `config/administrators.yml` which has to be updated manually.
        Without updating the administrators info the application doesn't work properly.
    - Tiny refactor to improve naming

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -7,8 +7,26 @@ class NotificationsControllerTest < ActionController::TestCase
   end
 
   test "server error" do
-    get :server_error
-    assert_response 302
-  end
+    ENV['TWILIO_NUMBER'] = 'twilio-number'
+    client   = Minitest::Mock.new
+    messages = Minitest::Mock.new
 
+    alert_message = <<MSG
+[This is a test] ALERT!
+It appears the server is having issues.
+Exception: A test exception.
+Go to: http://newrelic.com for more details."
+MSG
+
+    messages.expect(:create, true, [{ from: ENV['TWILIO_NUMBER'], to: "+15553185056", body: alert_message }])
+    client.expect(:messages, messages)
+
+    Twilio::REST::Client.stub :new, client do
+      get :server_error
+      assert_response 302
+    end
+
+    client.verify
+    messages.verify
+  end
 end
