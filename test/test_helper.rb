4c8782f282d473d6ff215fe7c361df9d28d3e226
Agustin Camino <agustin.camino@gmail.com>
2016-10-12 08:38:01 -0500

    Remove database dependency
    This application does not require to use a database.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/mock'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
-end
+# class ActiveSupport::TestCase
+#   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+#   fixtures :all
+#
+#   # Add more helper methods to be used by all tests here...
+# end
